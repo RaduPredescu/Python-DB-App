fd40617c6641344a4ff25efa3a0ac184520b1aa4
Radu Predescu <radu637@gmail.com>
2023-12-30 16:12:08 +0200

    Added delete and modify for Magazin

--- DBApp.py
+++ DBApp.py
@@ -2,7 +2,7 @@ import tkinter as tk
 from tkinter import Label, Entry, Button,Listbox
 from client_utils import add_client_to_database, remove_client_from_database,update_client_to_database,view_client_from_database
 from tkinter import ttk
-from magazin_utils import add_magazin_to_database
+from magazin_utils import add_magazin_to_database,remove_magazin_from_database,update_magazin_to_database
 
 root = tk.Tk()
 root.title("Database App")
@@ -34,9 +34,14 @@ def update_client():
 def view_client():
     view_client_from_database(tree)
 
+def remove_magazin():
+    remove_magazin_from_database(id_magazin_delete_entry)
+
 def add_magazin():
     add_magazin_to_database(id_magazin_entry,nume_magazin_entry,locatie_magazin_entry,tip_magazin_entry)
 
+def update_magazin():
+    update_magazin_to_database(id_magazin_entry_update,nume_magazin_entry_update,locatie_magazin_entry_update,tip_magazin_entry_update)
 
 #adaugare client
 Label(root, text="ID_client:").grid(row=0, column=0)
@@ -94,12 +99,12 @@ Label(root, text="Telefon to update: ").grid(row=4, column=2)
 telefon_client_entry_update = Entry(root)
 telefon_client_entry_update.grid(row=4, column=3)
 
-update_button_client = Button(root, text="Update client", command=update_client)
+update_button_client = Button(root, text="Update Client", command=update_client)
 update_button_client.grid(row=5, column=3)
 
 
 #vizualizare client
-view_button_client = Button(root, text="View client", command=view_client)
+view_button_client = Button(root, text="View Client", command=view_client)
 view_button_client.grid(row=7, column=3)
 
 tree = ttk.Treeview(root, columns=("ID_client", "Nume", "Prenume", "CNP", "Telefon"), show="headings", height=5)
@@ -114,7 +119,7 @@ tree.heading("Telefon", text="Telefon")
 
 
 #adaugare magazin
-Label(root,text="ID_mazagin: ").grid(row=11,column=0)
+Label(root,text="ID_magazin: ").grid(row=11,column=0)
 id_magazin_entry = Entry(root)
 id_magazin_entry.grid(row=11,column=1)
 
@@ -130,8 +135,38 @@ Label(root,text="Tip produse: ").grid(row=14,column=0)
 tip_magazin_entry = Entry(root)
 tip_magazin_entry.grid(row=14,column=1)
 
-add_button_magazin= Button(root, text="Add magazin to Database", command=add_magazin)
+add_button_magazin= Button(root, text="Add Magazin to Database", command=add_magazin)
 add_button_magazin.grid(row=15, column=1)
+
+
+#stergere magazin
+Label(root,text="ID_magazin to Remove: ").grid(row=16,column=0)
+id_magazin_delete_entry = Entry(root)
+id_magazin_delete_entry.grid(row=16,column=1)
+
+remove_button_magazin = Button(root,text="Remove Magazin from Database",command=remove_magazin)
+remove_button_magazin.grid(row=17,column=1)
+
+
+#update magazin
+Label(root,text="ID_magazin to update: ").grid(row=11,column=2)
+id_magazin_entry_update = Entry(root)
+id_magazin_entry_update.grid(row=11,column=3)
+
+Label(root,text="Nume magazin to update: ").grid(row=12,column=2)
+nume_magazin_entry_update = Entry(root)
+nume_magazin_entry_update.grid(row=12,column=3)
+
+Label(root,text="Locatie to update: ").grid(row=13,column=2)
+locatie_magazin_entry_update = Entry(root)
+locatie_magazin_entry_update.grid(row=13,column=3)
+
+Label(root,text="Tip produse to update: ").grid(row=14,column=2)
+tip_magazin_entry_update = Entry(root)
+tip_magazin_entry_update.grid(row=14,column=3)
+
+update_button_magazin = Button(root, text="Update Magazin to Database", command=update_magazin)
+update_button_magazin.grid(row=15, column=3)
 
 
 root.mainloop()
--- magazin_utils.py
+++ magazin_utils.py
@@ -1,6 +1,7 @@
 from connect import connect_to_database
 from tkinter import messagebox
 
+
 def add_magazin_to_database(
     id_magazin_entry,
     nume_magazin_entry,
@@ -35,6 +36,66 @@ def add_magazin_to_database(
 
         print("Magazin added to the database")
         messagebox.showinfo("Success", "Magazin added to the database")
+
+    except Exception as e:
+        messagebox.showerror("Error", f"Error: {str(e)}")
+
+
+def remove_magazin_from_database(id_magazin_delete_entry):
+    try:
+        connection = connect_to_database()
+        cursor = connection.cursor()
+
+        id_magazin_str = id_magazin_delete_entry.get()
+
+        try:
+            id_magazin = int(id_magazin_str)
+        except ValueError:
+            messagebox.showerror("Error", "ID_magazin must be a valid integer")
+            return
+        
+        delete_query = "DELETE FROM magazine WHERE ID_magazin = %s"
+        cursor.execute(delete_query, (id_magazin,))
+
+        connection.commit()
+        cursor.close()
+        connection.close()
+
+        print("Client removed from the database")
+        messagebox.showinfo("Success", "Client removed from the database")
+
+    except Exception as e:
+        messagebox.showerror("Error", f"Error: {str(e)}")
+
+def update_magazin_to_database(id_magazin_entry_update,nume_magazin_entry_update,locatie_magazin_entry_update,tip_magazin_entry_update):
+    try:
+        connection = connect_to_database()
+        cursor = connection.cursor()
+
+        id_magazin_str = id_magazin_entry_update.get()
+        nume_magazin = nume_magazin_entry_update.get()
+        locatie_magazin = locatie_magazin_entry_update.get()
+        tip_magazin = tip_magazin_entry_update.get()
+
+        try:
+            id_magazin = int(id_magazin_str)
+        except ValueError:
+            messagebox.showerror("Error", "ID_client must be a valid integer")
+            return
+
+        update_query = "UPDATE magazine SET nume_magazin = %s, locatie = %s, tip_produse = %s WHERE ID_magazin = %s"
+        cursor.execute(
+            update_query,
+            (nume_magazin, locatie_magazin, tip_magazin, id_magazin),
+        )
+
+        connection.commit()
+
+        cursor.close()
+        connection.close()
+
+        print("Magazin modified from database")
+        messagebox.showinfo("Success", "Magazin modified from database")
 
     except Exception as e:
         messagebox.showerror("Error", f"Error: {str(e)}")
